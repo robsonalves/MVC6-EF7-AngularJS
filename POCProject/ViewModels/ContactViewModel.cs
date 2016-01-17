142846a9ca3810cc299654ded571355f1b12c2e2
Robson Alves <robsonalves@robsonalves.net>
2016-01-17 02:38:57 -0200

    Developing new solution with mvc 6

---
+++ POCProject/ViewModels/ContactViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POCProject.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(1024, MinimumLength = 5)]
+        public string Message { get; set; }
+    }
+}
